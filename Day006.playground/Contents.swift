251951db6a333f084e242ffe97d5cf2d596f10d5
Jerozan Jeyendrarasa <me@jerojeyen.com>
2021-04-05 22:41:51 +0200

    Day 6/100 - done

---
+++ Day006.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+let driving2 = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+driving2("London")
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("London")
+print(message)
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
+travel {
+    print("I'm driving in my car")
+}
