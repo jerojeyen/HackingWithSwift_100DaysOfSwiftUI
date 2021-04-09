f32087c56b5434b0d2f0cde5e2bf8eb0c19f368d
Jerozan Jeyendrarasa <me@jerojeyen.com>
2021-04-09 23:36:28 +0200

    Day 9/100 - done

---
+++ Day009.playground/Contents.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+struct User {
+    var username: String
+
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+var user = User()
+user.username = "twostraws"
+
+struct Employee {
+    var name: String
+    var yearsActive = 0
+}
+
+extension Employee {
+    init() {
+        self.name = "Anonymous"
+        print("Creating an anonymous employee…")
+    }
+}
+
+// creating a named employee now works
+let roslin = Employee(name: "Laura Roslin")
+
+// as does creating an anonymous employee
+let anon = Employee()
+
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person2 {
+    var name: String
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person2(name: "Ed")
+
+ed.familyTree
+
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+let bob = Student(name: "Bob")
+let taylor = Student(name: "Taylor")
+
+print(Student.classSize)
+
+struct Person4 {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+    
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+
+let ed2 = Person4(id: "12345")
+print(ed2.identify())
