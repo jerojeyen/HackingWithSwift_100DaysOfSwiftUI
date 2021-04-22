46419ec25d01758333dfca08f8b6c6c17765a31c
Jerozan Jeyendrarasa <me@jerojeyen.com>
2021-04-22 22:46:26 +0200

    Day 19/100 - Init LengthConverter app

---
+++ LenghtConverter/LenghtConverter/LenghtConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenghtConverterApp.swift
+//  LenghtConverter
+//
+//  Created by Jerozan Jeyendrarasa on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LenghtConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
